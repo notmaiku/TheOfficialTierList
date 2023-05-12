de991d56c739830e48928c99ce20034f413cc840
notMaiku <mike1davis@rocketmail.com>
2023-05-12 03:30:52 -0400

    fix the padding issue

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,11 +3,13 @@ import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import { env } from 'env/enviroment';
+import cors from 'cors';
+import { HttpHeaders } from '@angular/common/http';
 
 const uri = env.baseurl
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({uri}),
+    link: httpLink.create({uri, withCredentials: true}),
     cache: new InMemoryCache({
         addTypename: false
     }),
